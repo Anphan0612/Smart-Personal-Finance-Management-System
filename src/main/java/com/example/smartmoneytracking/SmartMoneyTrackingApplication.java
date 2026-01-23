800aba42bbbd3ad64f9567d5fdbfb5748a21375c
Tiendat16 <134023823+tiendat1631@users.noreply.github.com>
2026-01-23 11:39:04 +0700

    Initial commit: Upload project source code

---
+++ src/main/java/com/example/smartmoneytracking/SmartMoneyTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.example.smartmoneytracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartMoneyTrackingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartMoneyTrackingApplication.class, args);
+    }
+
+}
