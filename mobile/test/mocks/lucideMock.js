dcc4e399fe3213b05477fe80cdc348d19a544f3b
Anphan0612 <phanquocan0612@gmail.com>
2026-04-26 00:25:14 +0700

    test(mobile): add tests for mobile features

---
+++ mobile/test/mocks/lucideMock.js
@@ -0,0 +1,12 @@
+module.exports = {
+  Mail: () => null,
+  Lock: () => null,
+  Eye: () => null,
+  EyeOff: () => null,
+  X: () => null,
+  FileText: () => null,
+  Check: () => null,
+  Wallet: () => null,
+  ArrowRight: () => null,
+  Info: () => null,
+};
